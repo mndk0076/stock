6b202a735be23825989f047404b5e7ec3e647658
Lawrence Tillson <ltillson7@gmail.com>
2019-03-15 15:05:14 -0400

    Lawrence Features

---
+++ includes/projectionChart.php
@@ -0,0 +1,11 @@
+<?php
+include "../../PHPProjectStock/stock/includes/header.php";
+?>
+<link rel="stylesheet" href="../css/projectionstyle.css">
+<main id="main">
+
+    <img id="graphImage" src="../images/placeholder_chart.png" alt="Placeholder Chart"/>
+
+</main>
+<?php
+include "../../PHPProjectStock/stock/includes/footer.php";
