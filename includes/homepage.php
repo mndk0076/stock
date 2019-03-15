94774459b8826b89dbc1694db316477807b3da9a
Kenneth <Kenneth@DESKTOP-FMJD8GD>
2019-03-15 17:16:21 -0400

    added homepage

---
+++ includes/homepage.php
@@ -0,0 +1,97 @@
+<?php 
+    require_once 'header.php';
+?>
+    <div class="home-container">
+        <div class="home-portfolio">
+            <h3 class="home-portfolio-title">Portfolio</h3>
+            <i class="fas fa-cog home-icon"></i>
+            <table class="table-hover">
+                <thead>
+                    <tr>
+                        <th>Ticker</th>
+                        <th>Company</th>
+                        <th>Shares</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>APPL</td>
+                        <td>Apple Inc</td>
+                        <td>10</td>
+                        <td>$154.3</td>
+                    </tr>
+                    <tr>
+                        <td>APPL</td>
+                        <td>Apple Inc</td>
+                        <td>10</td>
+                        <td>$154.3</td>
+                    </tr>
+                    <tr>
+                        <td>APPL</td>
+                        <td>Apple Inc</td>
+                        <td>10</td>
+                        <td>$154.3</td>
+                    </tr>
+                    <tr>
+                        <td>APPL</td>
+                        <td>Apple Inc</td>
+                        <td>10</td>
+                        <td>$154.3</td>
+                    </tr>
+                    <tr>
+                        <td>APPL</td>
+                        <td>Apple Inc</td>
+                        <td>10</td>
+                        <td>$154.3</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="home-status">
+            <h2>PUT YOURS HERE ENRINA</h2>
+        </div>
+         <div class="home-watchlist">
+            <h3 class="home-portfolio-title">Watchlist</h3>
+            <i class="fas fa-cog home-icon"></i>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Ticker</th>
+                        <th>Company</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>APPL</td>
+                        <td>Apple Inc</td>
+                        <td>$154.3</td>
+                    </tr>
+                    <tr>
+                        <td>APPL</td>
+                        <td>Apple Inc</td>
+                        <td>$154.3</td>
+                    </tr>
+                    <tr>
+                        <td>APPL</td>
+                        <td>Apple Inc</td>
+                        <td>$154.3</td>
+                    </tr>
+                    <tr>
+                        <td>APPL</td>
+                        <td>Apple Inc</td>
+                        <td>$154.3</td>
+                    </tr>
+                    <tr>
+                        <td>APPL</td>
+                        <td>Apple Inc</td>
+                        <td>$154.3</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+<?php 
+    include 'footer.php';
+?>
